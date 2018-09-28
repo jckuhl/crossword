8e7d9a267f6d8ec3d25ae76bde86f1ab30e6c659
Jonathan Kuhl <jonathankuhl@Jonathans-MBP-2.maine.rr.com>
2018-09-28 16:18:12 -0400

    logic to fetch answers and letters is complete

--- src/script/app.js
+++ src/script/app.js
@@ -1,5 +1,34 @@
+function getWords() {
+    return fetch('https://words-project-breakpoint.firebaseio.com/words.json')
+        .then(data => data.json())
+        .then(json => json)
+        .catch(error => console.log(error));
+}
 
-fetch('https://words-project-breakpoint.firebaseio.com/words.json')
-    .then(data=> data.json())
-    .then(json=> console.log(json));
+function getRandomWords(words, number) {
+    const randomWords = [];
+    let count = 0;
+    const random = ()=> Math.floor(Math.random() * (words.length-1));
+    while(count < number) {
+        randomWords.push(words[random()]);
+        count++;
+    }
+    return randomWords
+}
 
+function getPuzzleLetters(words) {
+    return new Set(words.join('').split('').sort());
+}
+
+async function getPuzzleAnswers({len, number}) {
+    let words = (await getWords()).filter(word => {
+        if(word.length > 4 && word.length < len) {
+            return word;
+        }
+    });
+    words = getRandomWords(words, number)
+    console.log(getPuzzleLetters(words));
+    return words
+}
+
+const answers = getPuzzleAnswers({ len: 8, number: 5})
