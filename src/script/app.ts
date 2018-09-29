457c751b9709bbf0c12111bd4b6a805c73563fa2
Jonathan Kuhl <jonathankuhl@Jonathans-MBP-2.maine.rr.com>
2018-09-29 00:35:04 -0400

    committing before adding frequency algorthm

--- src/script/app.ts
+++ src/script/app.ts
@@ -4,15 +4,23 @@ class WordGame {
     private letters: string[];
     private numLetters: number;
     private ABC = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    private ABC_FREQ = {
+        e: 11.1607, a: 8.4966, r: 7.5809, i: 7.5448,
+        o: 7.1635, t: 6.9509, n: 6.6544, s: 5.7351,
+        l: 5.4893, c: 4.5388, u: 3.6308, d: 3.3844,
+        p: 3.1671, m: 3.0129, h: 3.0034, g: 2.4705,
+        b: 2.0720, f: 1.8121, y: 1.7779, w: 1.2899,
+        k: 1.1016, v: 1.0074, x: 0.2902, z: 0.2722,
+        j: 0.1965, q: 0.1962
+    }
 
     constructor(numLetters: number) {
         this.numLetters = numLetters;
         this.letters = this.getRandomLetters();
-    } 
+    }
     
-
     private getRandomLetters(): string[] {
-        const random = ()=> Math.floor(Math.random() * 26);
+        const random = ()=> Math.floor(Math.random() * this.ABC.length - 1);
         const letters = [];
         for(let x = 0; x < this.numLetters; x++) {
             letters.push(this.ABC[random()])
@@ -27,9 +35,21 @@ class WordGame {
             .catch(error => console.log(error));
     }
 
+    private wordIsPossible(word: string): boolean {
+        let possible = true;
+        const letters = word.split('');
+        for(let letter of letters) {
+            if(this.letters.indexOf(letter) == -1) {
+                possible = false;
+            }
+        }
+        return possible;
+    }
+
     async startGame() {
         this.words = (await this.getWords())
-        console.log(this.letters);
+            .filter(word=> this.wordIsPossible(word) && word.length > 3 ? word : false)
+            .sort();
         console.log(this.words);
     }
 
